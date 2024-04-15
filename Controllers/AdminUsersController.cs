c284cad25d7b4e48c37f8adf4842125100ede974
Asteek Goswami <asteek.goswami@tmotions.com>
2024-04-15 22:35:18 +0530

    Create user feature add

--- Controllers/AdminUsersController.cs
+++ Controllers/AdminUsersController.cs
@@ -1,6 +1,7 @@
 ﻿using CodeBlog.Models.ViewModels;
 using CodeBlog.Repositories.Interface;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodeBlog.Controllers
@@ -9,11 +10,15 @@ namespace CodeBlog.Controllers
 	public class AdminUsersController : Controller
 	{
 		private readonly IUserRespository userRespository;
+		private readonly UserManager<IdentityUser> userManager;
 
-		public AdminUsersController(IUserRespository userRespository)
+		public AdminUsersController(IUserRespository userRespository,UserManager<IdentityUser> userManager)
         {
 			this.userRespository = userRespository;
+			this.userManager = userManager;
 		}
+
+		[HttpGet]
         public async Task<IActionResult> List()
 		{
 			var users = await userRespository.GetAll();
@@ -34,5 +39,58 @@ namespace CodeBlog.Controllers
 
 			return View(userViewModel);
 		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> List(UserViewModel request)
+		{
+			var identityUser = new IdentityUser
+			{
+				UserName = request.Username,
+				Email = request.Email,
+
+			};
+
+			var identityResult = await userManager.CreateAsync(identityUser,request.Password);
+
+			if(identityResult is not null)
+			{
+				if(identityResult.Succeeded)
+				{
+					//assign roles to this user
+					var roles = new List<string> { "User" };
+
+					if (request.AdminRoleCheckbox)
+					{
+						roles.Add("Admin");
+					}
+
+					 identityResult = await userManager.AddToRolesAsync(identityUser, roles);
+
+
+					if(identityResult is not null && identityResult.Succeeded)
+					{
+						return RedirectToAction("List", "AdminUsers");
+					}
+				}
+			}
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(Guid id)
+		{
+			var user = await userManager.FindByIdAsync(id.ToString());
+			if(user is not null)
+			{
+				var identityResult = await userManager.DeleteAsync(user);
+				if(identityResult is not null)
+				{
+					return RedirectToAction("List", "AdminUsers");
+				}
+			}
+			return View();
+		}
+
 	}
 }
